49f1184ed26deb3f831fb16274b0b13c6d7887a5
ianshimabukuro <shimakun000@gmail.com>
2024-05-13 02:17:57 +0900

    Show only closing price of every day in the plot

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ current_value = 20
 t_period = 2000
 n_investors = 500
 list_of_investors = init_investor_list(n_investors)
-
 stock_evolution = []
+
 for t in range(0,t_period):
     for investor in list_of_investors:
         current_perception = investor.evaluate(intrinsic_value)
@@ -15,6 +15,9 @@ for t in range(0,t_period):
         last = investor.def_buysellprice(current_price=current_value, buy_sell=buy_or_sell)
         current_value = last[1]
     stock_evolution.append(current_value)
+
+
+
 plt.plot(stock_evolution)
 plt.show()
 print(stock_evolution)
